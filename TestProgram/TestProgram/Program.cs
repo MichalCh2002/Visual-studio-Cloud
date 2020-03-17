8eb49b9f05a4a75fb41435aabddba61bf69792e9
Michal Chamrád <chamrad@spsejecna.cz>
2020-03-17 12:17:51 +0100

    změna komentu

--- TestProgram/TestProgram/Program.cs
+++ TestProgram/TestProgram/Program.cs
@@ -7,7 +7,7 @@ namespace TestProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //new comment 
+            //new comment konečně
         }
     }
 }
