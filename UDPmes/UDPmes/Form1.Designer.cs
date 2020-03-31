f826d8e42110a6c15e347f30d562f4800e067365
Michal Chamrád <chamrad@spsejecna.cz>
2020-03-31 18:55:17 +0200

    Záloha projektu

---
+++ UDPmes/UDPmes/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace UDPmes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanelAll = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanelTop = new System.Windows.Forms.TableLayoutPanel();
+            this.labelZprava = new System.Windows.Forms.Label();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.textIn = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.tableLayoutPanelAll.SuspendLayout();
+            this.tableLayoutPanelTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelAll
+            // 
+            this.tableLayoutPanelAll.ColumnCount = 1;
+            this.tableLayoutPanelAll.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelAll.Controls.Add(this.tableLayoutPanelTop, 0, 0);
+            this.tableLayoutPanelAll.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanelAll.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelAll.Name = "tableLayoutPanelAll";
+            this.tableLayoutPanelAll.RowCount = 1;
+            this.tableLayoutPanelAll.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelAll.Size = new System.Drawing.Size(682, 210);
+            this.tableLayoutPanelAll.TabIndex = 0;
+            // 
+            // tableLayoutPanelTop
+            // 
+            this.tableLayoutPanelTop.BackColor = System.Drawing.Color.White;
+            this.tableLayoutPanelTop.ColumnCount = 2;
+            this.tableLayoutPanelTop.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelTop.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanelTop.Controls.Add(this.labelZprava, 0, 1);
+            this.tableLayoutPanelTop.Controls.Add(this.labelInfo, 1, 0);
+            this.tableLayoutPanelTop.Controls.Add(this.textIn, 1, 1);
+            this.tableLayoutPanelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanelTop.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanelTop.Name = "tableLayoutPanelTop";
+            this.tableLayoutPanelTop.RowCount = 2;
+            this.tableLayoutPanelTop.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.54902F));
+            this.tableLayoutPanelTop.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.45098F));
+            this.tableLayoutPanelTop.Size = new System.Drawing.Size(676, 204);
+            this.tableLayoutPanelTop.TabIndex = 0;
+            // 
+            // labelZprava
+            // 
+            this.labelZprava.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelZprava.AutoSize = true;
+            this.labelZprava.Location = new System.Drawing.Point(3, 141);
+            this.labelZprava.Name = "labelZprava";
+            this.labelZprava.Size = new System.Drawing.Size(129, 17);
+            this.labelZprava.TabIndex = 0;
+            this.labelZprava.Text = "Zpráva:";
+            this.labelZprava.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(138, 39);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(535, 17);
+            this.labelInfo.TabIndex = 1;
+            this.labelInfo.Text = "Napište zprávu, jenž chcete odeslat na server  ";
+            // 
+            // textIn
+            // 
+            this.textIn.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textIn.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textIn.Location = new System.Drawing.Point(138, 137);
+            this.textIn.Name = "textIn";
+            this.textIn.Size = new System.Drawing.Size(529, 25);
+            this.textIn.TabIndex = 2;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSend.Location = new System.Drawing.Point(482, 216);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(91, 25);
+            this.buttonSend.TabIndex = 1;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(579, 216);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(91, 25);
+            this.buttonCancel.TabIndex = 2;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.buttonSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(682, 253);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.tableLayoutPanelAll);
+            this.MaximumSize = new System.Drawing.Size(700, 300);
+            this.MinimumSize = new System.Drawing.Size(700, 300);
+            this.Name = "Form1";
+            this.Text = "UDP client";
+            this.tableLayoutPanelAll.ResumeLayout(false);
+            this.tableLayoutPanelTop.ResumeLayout(false);
+            this.tableLayoutPanelTop.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelAll;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelTop;
+        private System.Windows.Forms.Label labelZprava;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.TextBox textIn;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
+
---
+++ UDPmes/UDPmes/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPmes
+{
+    public partial class Form1 : Form
+    {
+
+        #region Konstruktor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+
+        #region cancelButton
+        /// <summary>
+        /// Tlačítko storno uzavře program
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); //Ukončení programu
+        }
+        #endregion
+
+
+        #region sendButton
+        /// <summary>
+        /// Odešle zadaný text na server
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            // 1. Vytvoří UDP přípojku (ipv4, datagram, UDP) 
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            // 2. Vytvoření objektu IPadress
+            IPAddress ip = IPAddress.Parse("193.85.203.188");
+
+            // 3. Vytvoření a připojení k EndPointu
+            IPEndPoint ipEnd = new IPEndPoint(ip, 11500);
+            s.Connect(ipEnd); 
+
+            // 4. Napsanou zprávu do pole bytů
+            string txt = textIn.Text;
+            byte[] zprava = Encoding.Default.GetBytes(txt);
+
+            // 5. Odešle zakódovaná data na server
+            s.Send(zprava);
+
+
+            //Kosmetické úpravy
+            textIn.Text = string.Empty;
+            textIn.Focus();
+        }
+        #endregion
+
+
+    }
+}
