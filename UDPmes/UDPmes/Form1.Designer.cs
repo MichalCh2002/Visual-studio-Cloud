3da39fa26ac42e7fa22522415e5b285a98abdb9b
Michal Chamrád <chamrad@spsejecna.cz>
2020-04-01 12:20:14 +0200

    Změna velikosti tlačítek

--- UDPmes/UDPmes/Form1.Designer.cs
+++ UDPmes/UDPmes/Form1.Designer.cs
@@ -46,10 +46,11 @@
             this.tableLayoutPanelAll.Controls.Add(this.tableLayoutPanelTop, 0, 0);
             this.tableLayoutPanelAll.Dock = System.Windows.Forms.DockStyle.Top;
             this.tableLayoutPanelAll.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelAll.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanelAll.Name = "tableLayoutPanelAll";
             this.tableLayoutPanelAll.RowCount = 1;
             this.tableLayoutPanelAll.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanelAll.Size = new System.Drawing.Size(682, 210);
+            this.tableLayoutPanelAll.Size = new System.Drawing.Size(513, 171);
             this.tableLayoutPanelAll.TabIndex = 0;
             // 
             // tableLayoutPanelTop
@@ -62,21 +63,23 @@
             this.tableLayoutPanelTop.Controls.Add(this.labelInfo, 1, 0);
             this.tableLayoutPanelTop.Controls.Add(this.textIn, 1, 1);
             this.tableLayoutPanelTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.tableLayoutPanelTop.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanelTop.Location = new System.Drawing.Point(2, 2);
+            this.tableLayoutPanelTop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanelTop.Name = "tableLayoutPanelTop";
             this.tableLayoutPanelTop.RowCount = 2;
             this.tableLayoutPanelTop.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.54902F));
             this.tableLayoutPanelTop.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.45098F));
-            this.tableLayoutPanelTop.Size = new System.Drawing.Size(676, 204);
+            this.tableLayoutPanelTop.Size = new System.Drawing.Size(509, 166);
             this.tableLayoutPanelTop.TabIndex = 0;
             // 
             // labelZprava
             // 
             this.labelZprava.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.labelZprava.AutoSize = true;
-            this.labelZprava.Location = new System.Drawing.Point(3, 141);
+            this.labelZprava.Location = new System.Drawing.Point(2, 114);
+            this.labelZprava.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelZprava.Name = "labelZprava";
-            this.labelZprava.Size = new System.Drawing.Size(129, 17);
+            this.labelZprava.Size = new System.Drawing.Size(97, 15);
             this.labelZprava.TabIndex = 0;
             this.labelZprava.Text = "Zpráva:";
             this.labelZprava.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -85,9 +88,10 @@
             // 
             this.labelInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.labelInfo.AutoSize = true;
-            this.labelInfo.Location = new System.Drawing.Point(138, 39);
+            this.labelInfo.Location = new System.Drawing.Point(103, 31);
+            this.labelInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelInfo.Name = "labelInfo";
-            this.labelInfo.Size = new System.Drawing.Size(535, 17);
+            this.labelInfo.Size = new System.Drawing.Size(404, 15);
             this.labelInfo.TabIndex = 1;
             this.labelInfo.Text = "Napište zprávu, jenž chcete odeslat na server  ";
             // 
@@ -95,17 +99,19 @@
             // 
             this.textIn.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.textIn.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textIn.Location = new System.Drawing.Point(138, 137);
+            this.textIn.Location = new System.Drawing.Point(103, 110);
+            this.textIn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textIn.Name = "textIn";
-            this.textIn.Size = new System.Drawing.Size(529, 25);
+            this.textIn.Size = new System.Drawing.Size(398, 23);
             this.textIn.TabIndex = 2;
             // 
             // buttonSend
             // 
             this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonSend.Location = new System.Drawing.Point(482, 216);
+            this.buttonSend.Location = new System.Drawing.Point(362, 176);
+            this.buttonSend.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonSend.Name = "buttonSend";
-            this.buttonSend.Size = new System.Drawing.Size(91, 25);
+            this.buttonSend.Size = new System.Drawing.Size(68, 23);
             this.buttonSend.TabIndex = 1;
             this.buttonSend.Text = "Send";
             this.buttonSend.UseVisualStyleBackColor = true;
@@ -115,9 +121,10 @@
             // 
             this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(579, 216);
+            this.buttonCancel.Location = new System.Drawing.Point(434, 176);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(91, 25);
+            this.buttonCancel.Size = new System.Drawing.Size(68, 23);
             this.buttonCancel.TabIndex = 2;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
@@ -126,15 +133,16 @@
             // Form1
             // 
             this.AcceptButton = this.buttonSend;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(682, 253);
+            this.ClientSize = new System.Drawing.Size(513, 210);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonSend);
             this.Controls.Add(this.tableLayoutPanelAll);
-            this.MaximumSize = new System.Drawing.Size(700, 300);
-            this.MinimumSize = new System.Drawing.Size(700, 300);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(529, 251);
+            this.MinimumSize = new System.Drawing.Size(529, 251);
             this.Name = "Form1";
             this.Text = "UDP client";
             this.tableLayoutPanelAll.ResumeLayout(false);
